21fa0a5f695efd29cd131aff57521d0b38d30964
Vasilis Totosis <billtotosis@gmail.com>
2026-08-15 13:00:01 +0200

    M6b: register DebugAdapterDescriptorFactory and DebugConfigurationProvider

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -1,7 +1,34 @@
+import * as fs from "fs";
 import * as vscode from "vscode";
 
-export function activate(_context: vscode.ExtensionContext) {
-  // Real registration logic lands in Task 2.
+export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.debug.registerDebugAdapterDescriptorFactory("vbastudio", {
+      createDebugAdapterDescriptor(): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
+        const serverPath = vscode.workspace
+          .getConfiguration("vbastudio")
+          .get<string>("dapServerPath", "");
+        return new vscode.DebugAdapterExecutable(serverPath, []);
+      },
+    }),
+    vscode.debug.registerDebugConfigurationProvider("vbastudio", {
+      resolveDebugConfiguration(
+        _folder: vscode.WorkspaceFolder | undefined,
+        config: vscode.DebugConfiguration
+      ): vscode.ProviderResult<vscode.DebugConfiguration> {
+        const serverPath = vscode.workspace
+          .getConfiguration("vbastudio")
+          .get<string>("dapServerPath", "");
+        if (!serverPath || !fs.existsSync(serverPath)) {
+          vscode.window.showErrorMessage(
+            "vbastudio.dapServerPath is not set to a valid VbaStudio.DapServer.exe. Set it in Settings before debugging."
+          );
+          return undefined;
+        }
+        return config;
+      },
+    })
+  );
 }
 
 export function deactivate() {}
