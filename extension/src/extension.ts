53310f911b6e18c1ccac604ea28b540342cd28ef
Vasilis Totosis <billtotosis@gmail.com>
2026-08-15 12:57:38 +0200

    M6b: scaffold VS Code extension, debugger contribution, and dapServerPath setting

---
+++ extension/src/extension.ts
@@ -0,0 +1,7 @@
+import * as vscode from "vscode";
+
+export function activate(_context: vscode.ExtensionContext) {
+  // Real registration logic lands in Task 2.
+}
+
+export function deactivate() {}
