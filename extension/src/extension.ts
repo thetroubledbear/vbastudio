838f73a1e453a0fed147fce48b2f0a4c0dbc9aa9
Vasilis Totosis <billtotosis@gmail.com>
2026-08-15 13:34:34 +0200

    M6c: wire Configure Launch Target and Set DapServer.exe Path commands

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -1,33 +1,36 @@
-import * as fs from "fs";
+// extension/src/extension.ts
 import * as vscode from "vscode";
+import { getDapServerPath, isValidServerPath, promptToBrowseForServer } from "./config";
+import { configureLaunch } from "./configureLaunch";
 
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.debug.registerDebugAdapterDescriptorFactory("vbastudio", {
       createDebugAdapterDescriptor(): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
-        const serverPath = vscode.workspace
-          .getConfiguration("vbastudio")
-          .get<string>("dapServerPath", "");
-        return new vscode.DebugAdapterExecutable(serverPath, []);
+        return new vscode.DebugAdapterExecutable(getDapServerPath(), []);
       },
     }),
     vscode.debug.registerDebugConfigurationProvider("vbastudio", {
-      resolveDebugConfiguration(
+      async resolveDebugConfiguration(
         _folder: vscode.WorkspaceFolder | undefined,
         config: vscode.DebugConfiguration
-      ): vscode.ProviderResult<vscode.DebugConfiguration> {
-        const serverPath = vscode.workspace
-          .getConfiguration("vbastudio")
-          .get<string>("dapServerPath", "");
-        if (!serverPath || !fs.existsSync(serverPath)) {
-          vscode.window.showErrorMessage(
-            "vbastudio.dapServerPath is not set to a valid VbaStudio.DapServer.exe. Set it in Settings before debugging."
+      ): Promise<vscode.DebugConfiguration | undefined> {
+        const serverPath = getDapServerPath();
+        if (!isValidServerPath(serverPath)) {
+          const action = await vscode.window.showErrorMessage(
+            "vbastudio.dapServerPath is not set to a valid VbaStudio.DapServer.exe. Set it in Settings before debugging.",
+            "Browse..."
           );
+          if (action === "Browse...") {
+            await promptToBrowseForServer();
+          }
           return undefined;
         }
         return config;
       },
-    })
+    }),
+    vscode.commands.registerCommand("vbastudio.setDapServerPath", () => promptToBrowseForServer()),
+    vscode.commands.registerCommand("vbastudio.configureLaunch", () => configureLaunch())
   );
 }
 
